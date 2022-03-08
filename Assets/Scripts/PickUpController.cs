fe70339c40b05a48acbec66eafe089dce4b129a1
Morgan Houston <mmhouston6@live.com>
2022-03-08 14:01:35 -0600

    SlotFull bug

--- Assets/Scripts/PickUpController.cs
+++ Assets/Scripts/PickUpController.cs
@@ -24,6 +24,7 @@ public class PickUpController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        slotFull = false;
         if (equipped)
         {
             itemRB = weaponContainer.GetComponentInChildren<Rigidbody>();
